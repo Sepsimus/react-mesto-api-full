92223a1c25f2876639e033da33b92909d304cf79
Sepsimus <kostya2120@yandex.ru>
2021-06-22 15:57:48 +0300

    Backend Test 1.8

--- backend/app.js
+++ backend/app.js
@@ -9,12 +9,12 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 // const { PORT = 3000 } = process.env;
 
 const app = express();
-/* app.use((req, res, next) => {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://domainname.kostya2120.nomoredomains.club');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   next();
-}); */
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
