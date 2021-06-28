d31ad3ef97d349fc5ef89a7f80a8be1bdaa6547f
Sepsimus <kostya2120@yandex.ru>
2021-06-28 04:24:01 +0300

    Project 15: Thirt Push 3.4

--- backend/app.js
+++ backend/app.js
@@ -16,12 +16,6 @@ const app = express();
 
 app.use(cors({ origin: 'http://api.domainname.kostya2120.nomoredomains.club', credentials: true }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
