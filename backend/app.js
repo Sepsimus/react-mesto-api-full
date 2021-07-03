7f3f69b96c039c996d16c769f29500615c033ae6
Sepsimus <kostya2120@yandex.ru>
2021-07-03 20:37:25 +0300

    15 Project: Forth Push 4.3.6

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,12 @@ app.use(cors({
   methods: 'GET,POST,PATCH,DELETE,OPTIONS,PUT',
 }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://domainname.kostya2120.nomoredomains.club');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 /* app.use((req, res, next) => {
   res.setHeader('Cash-Control', 'no-store');
   next();
